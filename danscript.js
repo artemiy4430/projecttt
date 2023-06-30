385d22be92e754d9b5e529d38a8ebfaef2d7278b
artemiy4430 <115292643+artemiy4430@users.noreply.github.com>
2023-06-30 19:28:00 +0300

    Add files via upload

---
+++ danscript.js
@@ -0,0 +1,23 @@
+// const likeButton = document.getElementById('likeButton');
+// const likeCount = document.getElementById('likeCount');
+// let count = 0;
+// likeButton.addEventListener('click', function() {
+//   count++;
+//   likeCount.textContent = count.toString();
+// });
+
+
+let likes = document.querySelectorAll('.S_likes-wrap');
+let likesCount;
+likes.forEach((like,index) => {
+  like.addEventListener('click', () => {
+    if(like.classList.contains('active')){
+      like.classList.remove('active');
+      console.log(`${index} --`)
+    }else{
+      like.classList.add('active');
+      console.log(`${index} ++`)
+    }
+    console.log(`${index} !`)
+  });
+});
